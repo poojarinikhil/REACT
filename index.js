6e682c06466abd9c5a383e49371ab11bfaa22e56
Nikhil poojari <poojarinikhil01@gmail.com>
2022-09-09 21:43:39 +0530

    n

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //ReactDOM.render(<ul><li>idli</li><li>vada pav</li></ul>, document.getElementById("root")) 
-ReactDOM.render(
-    <div>
-        <MainContent/>
-    </div>,document.getElementById('root')
-)
-function MainContent(){
-    return(
-        <h1>I'm learning React</h1>
-    )
-}
+// ReactDOM.render(
+//     <div>
+//         <MainContent/>
+//     </div>,document.getElementById('root')
+// )
+// function MainContent(){
+//     return(
+//         <h1>I'm learning React</h1>
+//     )
+// }
+ReactDOM.render(<h1 className="Greeting">hi i am nikhil poojari</h1>,document.getElementById("root"))
+ReactDOM.render(<h1>shut the fuck up</h1>,document.getElementById("root"))
