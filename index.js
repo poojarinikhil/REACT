4d8260a44b2b0a8d02eee25a8e66bcdac3f36dde
Nikhil poojari <poojarinikhil01@gmail.com>
2022-09-09 16:27:51 +0530

    practice

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-ReactDOM.render(<ul><li>idli</li><li>vada pav</li></ul>, document.getElementById("root")) 
+//ReactDOM.render(<ul><li>idli</li><li>vada pav</li></ul>, document.getElementById("root")) 
+ReactDOM.render(
+    <div>
+        <MainContent/>
+    </div>,document.getElementById('root')
+)
+function MainContent(){
+    return(
+        <h1>I'm learning React</h1>
+    )
+}
