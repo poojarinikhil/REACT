3d468329cd99a497e59f0d929bb6d92e1702954a
Nikhil poojari <poojarinikhil01@gmail.com>
2022-09-17 13:34:05 +0530

    n

--- index.js
+++ index.js
@@ -9,15 +9,41 @@
 //         <h1>I'm learning React</h1>
 //     )
 // }
-const  nav=(
-    <div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="50px"></img>
-        <h1>Fun facts about React</h1>
-        <ol>
-            <li>I also dont know</li>
-            <li>Learning this now</li>
-            <li>Seem fine till now</li>
-        </ol>
-    </div>
-)
-ReactDOM.render(nav,document.getElementById("root"))
+// const  nav=(
+//     <div>
+//         <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" height="50px"></img>
+//         <h1>Fun facts about React</h1>
+//         <ol>
+//             <li>I also dont know</li>
+//             <li>Learning this now</li>
+//             <li>Seem fine till now</li>
+//         </ol>
+//     </div>
+// )
+// ReactDOM.render(nav,document.getElementById("root"))
+function Header(){
+    return(
+        <header>
+            <nav>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="reactimg" height = "50px"/>
+                <h1>Reason to like react js</h1>
+            </nav>
+        </header>
+    )
+}
+function Unorderlist(){
+    return(
+        <div>
+        <Header/>
+        <ul>
+            <li>it is fun to learning</li>
+            <li>nice things we can learn</li>
+            <li>I got to get a job</li>
+        </ul>
+        <footer>
+            <p><small>@poojari development. All rights reserved</small></p>
+        </footer>
+        </div>
+    )
+}
+ReactDOM.render(<Unorderlist/>,document.getElementById("root"))
