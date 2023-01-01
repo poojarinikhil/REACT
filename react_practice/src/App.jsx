08a08c3b905e8c3978ab31653071f136363144fb
Nikhil poojari <poojarinikhil01@gmail.com>
2023-01-01 20:41:30 +0530

    gg

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Heading from "./Heading";
 import Para from "./Para";
-
+import Footer from "./Footer";
 
 function App(){
     return(
     <>
     <Heading></Heading>
     <Para/>
+    <Footer></Footer>
     </>
     )
 }
---
+++ react_practice/src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+
+const foot = {
+    backgroundColor: "blue",
+    color: "white"
+};
+function Footer(){
+    return <p style={foot}>created by @poojarinikhil</p>
+};
+export default Footer;
