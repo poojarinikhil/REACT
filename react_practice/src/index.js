9988b7f313929fe7050377216bcb2036d67849a2
Nikhil poojari <poojarinikhil01@gmail.com>
2023-01-01 16:14:19 +0530

    gg

--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -13,6 +13,7 @@ const localtime =date.toLocaleTimeString()
 const day = date.getDate()
 const month = date.getMonth()
 const year = date.getFullYear()
+const hour = date.getHours()
 const time = [date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()]
 const link = "https://csi.dbit.in"
 const heading={
@@ -22,31 +23,33 @@ const heading={
     fontWeight: "bold",
     fontFamily:" 'Dancing Script', cursive"
   }
+const hello ={
+  backgroundColor: "#50D0F4",
+  textAlign: "center",
+  borderRadius : "20px",
+  padding : "20px",
+  margin : "20px"
+}
+
+let greeting = "";
+const helloStyle = {
+};
+if (hour < 12) {
+  greeting = "Good Morning";
+  helloStyle.color = "Yellow";
+}
+else if (hour >= 12 && hour < 19){
+  greeting = "Good Afternoon";
+  helloStyle.color = "Green";
+}
+else if (hour > 19){
+  greeting = "Good Night";
+  helloStyle.color = "Blue"
+}
 // this is react fragment <> if we use div it creats extra space in website so we use react fragments
 ReactDOM.render(<> 
-    {/* challenge 1
-    <h2> This is my top 5 favourite Netflix show </h2>
-    <ol>
-        <li>Dark</li>
-        <p>this series is best of Netflix up to date. It is scifi series with a great plot and a mistery    
-        </p>
-        <li>Stranger things</li>
-        <p>It is also scifi series with interesting story and you will enjoy it.</p>
-        <li>Money heist</li>
-        <p>It is a great show that shows the story of group theifs about their heist S3 was not that good either it would be on the second slot</p>
-        <li>Behind her eyes</li>
-        <p>this show is literally so underated show this show has a great plot twist it would catch you off graud</p>
-        <li>Arcane</li>
-        <p>Arcane is also good show with the great characters and good animation.</p>
-    </ol> */}
-    <h1 style={{color : "cyan", textAlign: "center"}}>Hello, My name is {`${name} ${surname}`}</h1>
-    <h1 style={heading}>Hello, My name is {`${name} ${surname}`}</h1>
-    {/* <p>Todays date is {`${day}-${month}-${year}`}</p>
-    <p>Todays date is {`${localdate}`}</p>
-    <p> And Time is {time}</p>
-    <p> And Time is {localtime}</p> */}
-    <a href={link} target="_blank">
-    <img className="Image" src="https://picsum.photos/200/300" alt=""/>
-    </a>
+    <div style={hello}>
+      <h1>Hello Sir,<span style={helloStyle}>{greeting}</span></h1>
+    </div>
     </>,
     document.getElementById('root'));
