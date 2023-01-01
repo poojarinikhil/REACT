a188a831a17029e9bd0415df11e2ba1c64439114
Nikhil poojari <poojarinikhil01@gmail.com>
2023-01-01 21:04:01 +0530

    gg

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Heading from "./Heading";
 import Para from "./Para";
-import Footer from "./Footer";
+import {Footer,add,div,multi,sub} from "./Footer";
 
 function App(){
     return(
     <>
     <Heading></Heading>
     <Para/>
+    <ul>
+        <li>sum of two number is {add(40,45)}</li>
+        <li>sub of two number is {sub(40,45)}</li>
+        <li>mul of two number is {multi(40,45)}</li>
+        <li>div of two number is {div(40,45)}</li>
+    </ul>
     <Footer></Footer>
     </>
     )
--- react_practice/src/Footer.jsx
+++ react_practice/src/Footer.jsx
@@ -4,7 +4,23 @@ const foot = {
     backgroundColor: "blue",
     color: "white"
 };
+function add(num1,num2){
+    let sum = num1+num2;
+    return sum
+}
+function sub(num1,num2){
+    let minus = num1 - num2;
+    return minus
+}
+function multi(num1,num2){
+    let mul = num1*num2;
+    return mul
+}
+function div(num1,num2){
+    let divide = num1/num2;
+    return divide
+}
 function Footer(){
     return <p style={foot}>created by @poojarinikhil</p>
-};
-export default Footer;
+}
+export {add,Footer,div,multi,sub};
