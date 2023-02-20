757342611e9d772566f6dce7410634ce7936a828
Nikhil poojari <poojarinikhil01@gmail.com>
2023-02-20 21:43:39 +0530

    noice
    sdfasdfa

--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -1,11 +1,19 @@
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-// babel covert it into mordern javascrip v
-import React from "react"
-import ReactDOM  from "react-dom"
-import "./index.css"
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import Card from "./Cards";
+ReactDOM.render(
+    <>
+        <Card imgsrc="https://m.media-amazon.com/images/M/MV5BYTZmNTg3ZWYtZTQ1ZC00OTliLWFlNjQtODk1YzQ4NGI5ZmNlXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg" alt="" className="card_img"
+            title = "A Netflix Orignal Series"
+            sname = "DARK"
+            link = "https://www.netflix.com/in/title/80100172"
+        />
+        <Card imgsrc = "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f47afa77e1a3231b031ee828f2f38b2af2073a06f05e004bf53d5709c51c5e5c._RI_V_TTW_.jpg"
+        title = "Amazon Original Series"
+        sname = "The Boys"
+        link = "https://www.amazon.com/The-Boys-Season-1/dp/B0875THX53"
+        />
+    </>,
+    document.getElementById('root')
 
-
-// this is react fragment <> if we use div it creats extra space in website so we use react fragments
-ReactDOM.render(<App/>,document.getElementById('root'));
+)
