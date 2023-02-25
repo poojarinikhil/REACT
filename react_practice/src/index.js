83d0c24aa8caba9ca0d429be6b5fd700e4908d49
Nikhil poojari <poojarinikhil01@gmail.com>
2023-02-25 11:19:31 +0530

    nn

--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 import Card from "./Cards";
 import "./index.css";
 import Sdata from "./Sdata";
+function ncard(val){
+    return(
+        <Card imgsrc={val.imgsrc} alt="" className="card_img"
+            title = {val.title}
+            sname = {val.sname}
+            link = {val.link}
+        />
+    )
+}
 ReactDOM.render(
     <>
-        <Card imgsrc={Sdata[0].imgsrc} alt="" className="card_img"
-            title = {Sdata[0].title}
-            sname = {Sdata[0].sname}
-            link = {Sdata[0].link}
-        />
-        <Card imgsrc = {Sdata[1].imgsrc}
-        title = {Sdata[1].title}
-        sname = {Sdata[1].sname}
-        link = {Sdata[1].link}
-        />
+        {Sdata.map(ncard)}
     </>,
     document.getElementById('root')
 
